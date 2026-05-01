11f7aadada339d26dd73fae85d09c5e404cc8743
soumilgurjar951-gif <soumilgurjar951@gmail.com>
2026-05-01 08:57:59 +0530

    rainbowcolour.py

---
+++ rainbow.py
@@ -0,0 +1,15 @@
+R = (input("enter colour"))
+if(R=='v'):
+    print("voilet")
+elif(R=='i'):
+    print("indigo")
+elif(R=='b'):
+    print("blue")
+elif(R=='g'):
+    print("green")
+elif(R=='o'):
+    print("orange")
+elif(R=='y'):
+    print("yellow")
+else:
+    print("red")
